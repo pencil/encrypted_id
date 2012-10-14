d8ab69c6d93411e8c52c6ddf6f0692bdcc55de1d
Nils Caspar <ncaspar@me.com>
2012-10-14 13:45:11 +0200

    fixed typo

--- spec/encrypted_id_spec.rb
+++ spec/encrypted_id_spec.rb
@@ -11,7 +11,7 @@ describe 'EncryptedId' do
       @user.to_param.should == '1e8644f924812bec506b116ff14368c8'
     end
 
-    it 'should be possible to find an entry by the encrypred id' do
+    it 'should be possible to find an entry by the encrypted id' do
       @user.id = 8
       @user.save!
       User.find('da9f98cd7c3eb2b0f0e88cc8daeb222c').id.should == 8
@@ -34,7 +34,7 @@ describe 'EncryptedId' do
       @animal.to_param.should == '2f71ba22a8e6975db0f13b7e3db6d9cd'
     end
 
-    it 'should be possible to find an entry by the encrypred id' do
+    it 'should be possible to find an entry by the encrypted id' do
       @animal.id = 8
       @animal.save!
       Animal.find('6916a1adba452d3fffde6e444ae1ad3a').id.should == 8
