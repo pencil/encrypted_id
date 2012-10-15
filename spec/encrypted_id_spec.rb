52f3cb099a0e58ae7563c8e76366083c9a4f7f0b
Nils Caspar <ncaspar@me.com>
2012-10-15 17:49:04 +0200

    removed redundancy in specs

--- spec/encrypted_id_spec.rb
+++ spec/encrypted_id_spec.rb
@@ -1,49 +1,39 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe 'EncryptedId' do
-  describe 'in the User model' do
-    before(:each) do
-      @user = User.new
-    end
+  ModelDescription = Struct.new(:model, :test_ids)
 
-    it 'should give us the encrypted ID via to_param' do
-      @user.id = 15
-      @user.to_param.should == '1e8644f924812bec506b116ff14368c8'
-    end
+  [
+    ModelDescription.new(User,
+      { 15 => '1e8644f924812bec506b116ff14368c8', 8 => 'da9f98cd7c3eb2b0f0e88cc8daeb222c' }
+    ),
+    ModelDescription.new(Animal,
+      { 15 => '2f71ba22a8e6975db0f13b7e3db6d9cd', 8 => '6916a1adba452d3fffde6e444ae1ad3a' }
+    )
+  ].each do |description|
+    describe "in the #{description.model}.name model" do
+      before(:each) do
+        @model = description.model
+        @test_ids = description.test_ids
+        @entity = @model.new
+      end
 
-    it 'should be possible to find an entry by the encrypted id' do
-      @user.id = 8
-      @user.save!
-      User.find('da9f98cd7c3eb2b0f0e88cc8daeb222c').id.should == 8
-    end
+      it 'should give us the encrypted ID via to_param' do
+        @entity.id = 15
+        @entity.to_param.should == @test_ids[15]
+      end
 
-    it 'should throw an exception if we try to find an entry by the real ID' do
-      @user.id = 1
-      @user.save!
-      expect { User.find 1 }.to raise_error(ActiveRecord::RecordNotFound)
-    end
-  end
+      it 'should be possible to find an entry by the encrypted id' do
+        @entity.id = 8
+        @entity.save!
+        @model.find(@test_ids[8]).id.should == 8
+      end
 
-  describe 'in the Animal model' do
-    before(:each) do
-      @animal = Animal.new
-    end
-
-    it 'should give us the encrypted ID via to_param' do
-      @animal.id = 15
-      @animal.to_param.should == '2f71ba22a8e6975db0f13b7e3db6d9cd'
-    end
-
-    it 'should be possible to find an entry by the encrypted id' do
-      @animal.id = 8
-      @animal.save!
-      Animal.find('6916a1adba452d3fffde6e444ae1ad3a').id.should == 8
-    end
-
-    it 'should throw an exception if we try to find an entry by the real ID' do
-      @animal.id = 1
-      @animal.save!
-      expect { Animal.find 1 }.to raise_error(ActiveRecord::RecordNotFound)
+      it 'should throw an exception if we try to find an entry by the real ID' do
+        @entity.id = 1
+        @entity.save!
+        expect { @model.find 1 }.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
